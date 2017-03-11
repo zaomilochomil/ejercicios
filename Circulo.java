aeee49997c0a36abc8c7babfc8ea5f560f08e9b0
zaomilochomil <162t0310@itsm.edu.mx>
2017-03-11 16:33:38 -0600

    circle

--- Circulo.java
+++ Circulo.java
@@ -1,55 +1,53 @@
-public class Circulo {
+package circulo;
 
-    private double radius;
-    private String color;
-    
-    Circulo(){
-        
-    }
-    public Circulo(double radius, String color) {
-        this.radius = radius;
-        this.color = color;
-    }
+public class circulo {
+	private double radius;
+	private String color;
 
-    public double getRadius() {
-        return radius;
-    }
-
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    
-    
-
+	public circulo(){
+		radius = 1.0;
+		color = "red";
+	}
+	public circulo (double r ){
+		radius = r;	
+		color = "red";
+		}
+public double getRadius (){
+	return radius;
+}
+public double getArea (){
+	return radius*radius*Math.PI;
+	
+}
+public String getColor (){
+	return color;
+	
+}
 }
 aplicacion
+package circulo;
 
-public class Circle extends Circulo{
+public class Circle {
+public static void main (String [] args){
+	
 
-    public Circle() {
-    }
+	circulo c1 new = circulo(2,azul);
+	circulo c2 new = circulo(2,red);
+	circulo c3 new = circulo(1,red);
 
-    public Circle(double radius, String color) {
-        super(radius, color);
-    }
-
-    public double  radio(double radius){
-    return radius;
+			
+System.out.println ("el radio del circulo es: "
++ c1.getRadius() "su area es: " + c1.getArea()
+"su color es:" + c1.getColor() ;
+					
+			System.out.println ("el radio del circulo es: "
+			+ c2.getRadius() "su area es: " + c.getArea()
+			"su color es:" + c2.getColor() ;
+							
+			System.out.println ("el radio del circulo es: "
+			+ c3.getRadius() "su area es: " + c3.getArea()
+			"su color es:" + c3.getColor() ;
+									
+	
 }
-    
-    
-    public double area(double getRadius){
-        double Area=Math.PI*(getRadius*getRadius);
-        return Area;
-    }
-    
 }
